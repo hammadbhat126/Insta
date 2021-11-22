4e0daa91ad8221fa6cb386dacc205c1441995a7a
Hammad <hammad@rpi.ae>
2021-11-22 10:20:58 +0400

    Authentication

--- app/src/main/java/com/kashsoft/insta/SignInActivity.kt
+++ app/src/main/java/com/kashsoft/insta/SignInActivity.kt
@@ -1,11 +1,22 @@
 package com.kashsoft.insta
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.SignIn_link_btn
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        Signup_link_btn.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+
+
+        }
+
     }
 }
