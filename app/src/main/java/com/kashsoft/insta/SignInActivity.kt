74dde7bbbb540fe4123a79ceba8fcde2f0d27093
Hammad <hammad@rpi.ae>
2021-11-25 17:50:00 +0400

    Database

--- app/src/main/java/com/kashsoft/insta/SignInActivity.kt
+++ app/src/main/java/com/kashsoft/insta/SignInActivity.kt
@@ -1,35 +1,105 @@
 package com.kashsoft.insta
 
+
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
-import kotlinx.android.synthetic.main.activity_sign_up.SignIn_link_btn
+
 
 class SignInActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        Signup_link_btn.setOnClickListener {
-            startActivity(Intent(this,SignUpActivity::class.java))
 
-
+        signup_link_btn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
 
+        login_btn.setOnClickListener {
+            loginUser();
+        }
     }
+
+
+
+
+
+    private fun loginUser()
+    {
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        when
+        {
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "password is required.", Toast.LENGTH_LONG).show()
+
+            else ->
+            {
+                val progressDialog = ProgressDialog(this@SignInActivity)
+                progressDialog.setTitle("Login")
+                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful)
+                    {
+                        progressDialog.dismiss()
+
+                        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else
+                    {
+                        val message = task.exception!!.toString()
+                        Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
+                        FirebaseAuth.getInstance().signOut()
+                        progressDialog.dismiss()
+                    }
+                }
+            }
+        }
+    }
+
+
 
     override fun onStart() {
         super.onStart()
-        if (FirebaseAuth.getInstance().currentUser !=null)
+
+        if (FirebaseAuth.getInstance().currentUser != null)
         {
-          val intent =Intent(this@SignInActivity, MainActivity::class.java)
-          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or
-          Intent.FLAG_ACTIVITY_NEW_TASK)
-          finish()
+            val intent = Intent(this@SignInActivity, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
         }
+
+
+    val crashButton = Button(this)
+    crashButton.text = "Test Crash"
+    crashButton.setOnClickListener {
+        throw RuntimeException("Test Crash") // Force a crash
     }
+
+    addContentView(crashButton, ViewGroup.LayoutParams(
+    ViewGroup.LayoutParams.MATCH_PARENT,
+    ViewGroup.LayoutParams.WRAP_CONTENT))
+}
 }
