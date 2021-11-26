b831a3e73b03268e76b6140eb84acca92e2784c9
Hammad Yousuf <hammadbhat126@gmail.com>
2021-11-26 22:25:57 +0400

    Sign In

--- app/src/main/java/com/kashsoft/insta/SignInActivity.kt
+++ app/src/main/java/com/kashsoft/insta/SignInActivity.kt
@@ -28,7 +28,7 @@ class SignInActivity : AppCompatActivity() {
         }
 
         login_btn.setOnClickListener {
-            loginUser();
+            loginUser();      // method
         }
     }
 
@@ -36,70 +36,72 @@ class SignInActivity : AppCompatActivity() {
 
 
 
-    private fun loginUser()
-    {
+    private fun loginUser(){
+
         val email = email_login.text.toString()
         val password = password_login.text.toString()
 
-        when
-        {
-            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required.", Toast.LENGTH_LONG).show()
-            TextUtils.isEmpty(password) -> Toast.makeText(this, "password is required.", Toast.LENGTH_LONG).show()
+        when{
+
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required.",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this,"Password is Required.", Toast.LENGTH_LONG).show()
 
             else ->
             {
                 val progressDialog = ProgressDialog(this@SignInActivity)
                 progressDialog.setTitle("Login")
-                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setMessage("Please Wait, this take a while.... ")
                 progressDialog.setCanceledOnTouchOutside(false)
                 progressDialog.show()
 
                 val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
-
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
                     if (task.isSuccessful)
                     {
                         progressDialog.dismiss()
 
-                        val intent = Intent(this@SignInActivity, MainActivity::class.java)
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        val intent = Intent(this@SignInActivity , MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                         startActivity(intent)
                         finish()
                     }
-                    else
-                    {
+                    else{
                         val message = task.exception!!.toString()
                         Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
                         FirebaseAuth.getInstance().signOut()
                         progressDialog.dismiss()
                     }
                 }
+
             }
         }
+
+
     }
 
 
 
-    override fun onStart() {
-        super.onStart()
+ override fun onStart() {
+     super.onStart()
 
-        if (FirebaseAuth.getInstance().currentUser != null)
-        {
-            val intent = Intent(this@SignInActivity, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-            finish()
-        }
+     if (FirebaseAuth.getInstance().currentUser != null)
+     {
+         val intent = Intent(this@SignInActivity, MainActivity::class.java)
+         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+         startActivity(intent)
+         finish()
+     }
 
 
-    val crashButton = Button(this)
-    crashButton.text = "Test Crash"
-    crashButton.setOnClickListener {
-        throw RuntimeException("Test Crash") // Force a crash
-    }
+ val crashButton = Button(this)
+ crashButton.text = "Test Crash"
+ crashButton.setOnClickListener {
+     throw RuntimeException("Test Crash") // Force a crash
+ }
 
-    addContentView(crashButton, ViewGroup.LayoutParams(
-    ViewGroup.LayoutParams.MATCH_PARENT,
-    ViewGroup.LayoutParams.WRAP_CONTENT))
+ addContentView(crashButton, ViewGroup.LayoutParams(
+ ViewGroup.LayoutParams.MATCH_PARENT,
+ ViewGroup.LayoutParams.WRAP_CONTENT))
 }
+
 }
